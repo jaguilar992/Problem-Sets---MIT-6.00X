c1fcfeef041568ed4176c30b0cc28422a46e1ac2
jaguilar992 <jaguilar992@gmail.com>
2013-04-04 21:43:19 -0600

    Update StandardRobot.py

--- StandardRobot.py
+++ StandardRobot.py
@@ -1,4 +1,52 @@
-from ps7 import *
+import math
+import random
+
+import ps7_visualize
+import pylab
+from ps7_verify_movement27 import testRobotMovement
+class Position(object):
+    """
+    A Position represents a location in a two-dimensional room.
+    """
+    def __init__(self, x, y):
+        """
+        Initializes a position with coordinates (x, y).
+        """
+        self.x = x
+        self.y = y
+        
+    def getX(self):
+        return self.x
+    
+    def getY(self):
+        return self.y
+    
+    def getNewPosition(self, angle, speed):
+        """
+        Computes and returns the new Position after a single clock-tick has
+        passed, with this object as the current position, and with the
+        specified angle and speed.
+
+        Does NOT test whether the returned position fits inside the room.
+
+        angle: number representing angle in degrees, 0 <= angle < 360
+        speed: positive float representing speed
+
+        Returns: a Position object representing the new position.
+        """
+        old_x, old_y = self.getX(), self.getY()
+        angle = float(angle)
+        # Compute the change in position
+        delta_y = speed * math.cos(math.radians(angle))
+        delta_x = speed * math.sin(math.radians(angle))
+        # Add that to the existing position
+        new_x = old_x + delta_x
+        new_y = old_y + delta_y
+        return Position(new_x, new_y)
+
+    def __str__(self):  
+        return "(%0.2f, %0.2f)" % (self.x, self.y)
+
 class StandardRobot(Robot):
     """
     A StandardRobot is a Robot with the standard movement strategy.
@@ -14,9 +62,12 @@ class StandardRobot(Robot):
         Move the robot to a new position and mark the tile it is on as having
         been cleaned.
         """
-        pos=self.getRobotPosition()
-        self.room.cleanTileAtPosition(pos)
-        new_pos=pos.getNewPosition(self.angle, self.speed)
-        x,y=new_pos.getX(),new_pos.getY()
-        while xlabel:
-            pass
+        new_pos=self.position.getNewPosition(self.direction,self.speed)
+        if self.room.isPositionInRoom(new_pos):
+            self.room.cleanTileAtPosition(new_pos)
+            self.position=new_pos
+        else:
+            self.direction=360*random.random()
+
+#Uncomment this line to see your implementation of StandardRobot in action!
+testRobotMovement(StandardRobot, RectangularRoom)
